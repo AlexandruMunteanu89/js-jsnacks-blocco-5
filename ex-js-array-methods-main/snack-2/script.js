01d75e62907e1b1bccfcb54569b02966179313cf
AlexandruMunteanu89 <sasamunteanu18@gmail.com>
2026-04-01 14:40:33 +0200

    first commit

---
+++ ex-js-array-methods-main/snack-2/script.js
@@ -0,0 +1,12 @@
+const people = [
+  { name: 'Paolo', age: 35 },
+  { name: 'Giulia', age: 24 },
+  { name: 'Marco', age: 67 }
+];
+
+// Stampa in console tutti i nomi
+people.forEach(persone => {
+  console.log(persone.name);
+});
+
+// Risultato: 'Paolo', 'Giulia', 'Marco'
